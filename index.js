21253a968f11c7b39fd42bb17a006b4355655db7
Bruno Mota <bruno12mota@gmail.com>
2015-12-01 10:35:25 +0000

    default children for relax elements

--- index.js
+++ index.js
@@ -217,6 +217,7 @@ module.exports = {
                 "mixins",
                 "statics",
                 "defaultProps",
+                "defaultChildren",
                 "propsSchema",
                 "settings",
                 "style",
